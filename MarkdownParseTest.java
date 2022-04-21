50b551a3f1ddd22a311632d589107b0c3c154359
Ritviksiddha Penchala <ritviksiddha@gmail.com>
2022-04-21 10:56:04 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,9 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.*; // Import the JUnit tester code from the library
 import org.junit.*;
 
-public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+public class MarkdownParseTest { // Create the class
+    @Test // tell the code we're making a test here 
+    public void addition() { // the test method
+        assertEquals(2, 1 + 1); // check to see if 2 equals 1+1 and the test passes if it is
     }
 }

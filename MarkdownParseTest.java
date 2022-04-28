4bfa572505d41fc3fc62d0d934aaa71298b4a041
Ritviksiddha Penchala <ritviksiddha@gmail.com>
2022-04-28 11:45:03 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -11,7 +11,7 @@ public class MarkdownParseTest { // Create the class
 
     @Test // tell the code we're making a test here 
     public void addition() { // the test method
-        assertEquals(2, 1 + 1); // check to see if 2 equals 1+1 and the test passes if it is
+        assertEquals(3, 1 + 1); // check to see if 2 equals 1+1 and the test passes if it is
     }
 
     @Test

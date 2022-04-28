1e4318673cb0be985f711b58a89c2c74fee65d79
Ritviksiddha Penchala <ritviksiddha@gmail.com>
2022-04-28 10:35:44 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,15 @@
 import static org.junit.Assert.*; // Import the JUnit tester code from the library
 import org.junit.*;
+import java.util.List;
 
 public class MarkdownParseTest { // Create the class
     @Test // tell the code we're making a test here 
     public void addition() { // the test method
         assertEquals(2, 1 + 1); // check to see if 2 equals 1+1 and the test passes if it is
+    }
+
+    @Test
+    public void getLinksTest(){
+        assertEquals(MarkdownParse.main("test-file.md"), List.of("https://something.com", "some-thing.html"));
     }
 }
